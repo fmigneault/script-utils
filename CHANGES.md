a4f3462001fa1d3b2f3465f33600c0dfb82841ec
Francis Charette Migneault <francis.charette.migneault@gmail.com>
2020-04-21 18:21:24 -0400

    more args to control json/yaml convert + versioning

--- CHANGES.md
+++ CHANGES.md
@@ -5,9 +5,15 @@ Changes
 Unreleased
 ----------
 
+* Add ``--json`` and ``--yaml`` arguments to specify an output format not matching with the output file extension.
+* Add ``--ignore`` argument to ignore validation of input file extension. Will try parsing either JSON/YAML.
+* Add ``--version`` argument to keep track of any applied updates. 
+* Bump ``JSON <=> YAML`` convert tool to ``0.2.0``.
+* Fix current directory not used to call script from anywhere.
+
 0.1.0 (2020-04-21)
 ---------------------
 
 ### Changes
 
-* Add JSON <=> YAML conversion tool.
+* Add ``JSON <=> YAML`` conversion tool (version ``0.1.0`` but no ``--version`` argument yet to retrive it).
--- convert-tools/_json_yaml_converter.py
+++ convert-tools/_json_yaml_converter.py
@@ -6,23 +6,24 @@ import os
 import sys
 import yaml
 
-
-def read_content(file_path):
-    if file_path.endswith(".json"):
-        with open(file_path, 'r') as f:
-            return json.load(f)
-    if file_path.endswith(".yml") or file_path.endswith(".yaml"):
-        with open(file_path, 'r') as f:
-            return yaml.safe_load(f)
-    return None
+__version__ = "0.2.0"
 
 
-def write_content(data, file_path):
-    if file_path.endswith(".json"):
+def read_content(file_path, ignore_extension=False):
+    if not ignore_extension and not os.path.splitext(file_path) in [".json", ".yaml", ".yml"]:
+        return False
+    with open(file_path, 'r') as f:
+        return yaml.safe_load(f)    # can read both json/yaml
+
+
+def write_content(data, file_path, file_format=None):
+    if not file_format:
+        file_format = os.path.splitext(file_path)[-1].replace(".", "")
+    if file_format == "json":
         with open(file_path, 'w') as f:
             json.dump(data, f)
             return True
-    if file_path.endswith(".yml") or file_path.endswith(".yaml"):
+    if file_format in [".yaml", ".yml"]:
         with open(file_path, 'w') as f:
             yaml.safe_dump(f, f)
             return True
@@ -38,11 +39,19 @@ def main():
     ap = argparse.ArgumentParser(name, description="Tool that converts JSON<=>YAML", add_help=True)
     ap.add_argument("file_in", type=str, help="Input file from where to read data to convert (JSON or YAML).")
     ap.add_argument("file_out", type=str, help="Output file where to write converted data (JSON or YAML).")
+    ap.add_argument("--version", "-v", action="version", version="%(prog)s {}".format(__version__))
+    ap.add_argument("--ignore", "-i", action="store_true", dest="ignore_extension",
+                    help="Ignore input file extension, try to parse as either JSON or YAML.")
+    f_out = ap.add_mutually_exclusive_group()
+    f_out.add_argument("--json", "-j", action="store_const", const="json", dest="file_format", default="",
+                       help="Specify the desired output format as JSON. Useful when the desired extension is not JSON.")
+    f_out.add_argument("--yaml", "-y", "--yml", action="store_const", const="yaml", dest="file_format", default="",
+                       help="Specify the desired output format as YAML. Useful when the desired extension is not YAML.")
     args = ap.parse_args(args=args)
     if not args.file_in or not os.path.isfile(args.file_in):
         raise IOError("cannot reading: [{}]".format(args.file_in))
-    data = read_content(args.file_in)
-    success = write_content(data, args.file_out)
+    data = read_content(args.file_in, ignore_extension=args.ignore_extension)
+    success = write_content(data, args.file_out, file_format=args.file_format)
     if not success or not args.file_out or not os.path.isfile(args.file_out):
         raise IOError("failed writing: [{}]".format(args.file_out))
 
